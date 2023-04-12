668c11e150cf4d60be92dbf2ec8cb95ce032e903
paq1 <p.paquin1016@laposte.net>
2023-04-12 09:29:25 +0200

    feat: salt dans les venv

--- src/api/services/password_service_impl.rs
+++ src/api/services/password_service_impl.rs
@@ -7,30 +7,47 @@ impl PasswordServiceImpl {
     pub fn new() -> Self {
         Self {}
     }
+
+    pub fn salt_u8_16() -> [u8;16] {
+        Self::salt_string_reformatted(16)
+            .as_bytes()
+            .to_vec()[..16]
+            .try_into()
+            .unwrap()
+    }
+
+    pub fn salt_string_reformatted(length: u32) -> String {
+        let salt = std::env::var("SECRET_SALT")
+            .expect("Pas de sel dans les venv.");
+
+        Self::pad_with_zero(salt, length)
+    }
+
+    pub fn pad_with_zero(chaine: String, length: u32) -> String {
+        if chaine.len() >= length as usize {
+            chaine
+        } else {
+            let taille_manquante = length - chaine.len() as u32;
+            let fin_de_chaine = (0..taille_manquante)
+                .map(|_| "0".to_string())
+                .collect::<Vec<String>>()
+                .join("");
+            chaine.chars()
+                .chain(fin_de_chaine.chars())
+                .collect::<String>()
+        }
+    }
 }
 
 impl PasswordService for PasswordServiceImpl {
     fn create_hash_password(&self, password: String) -> String {
 
-        let salt: [u8;16] = "azertyuiopqsdfgh"
-            .as_bytes()
-            .to_vec()[..]
-            .try_into()
-            .unwrap();
-
-        // let exemple: [u8; 16] = [38, 113, 212, 141, 108, 213, 195, 166, 201, 38, 20, 13, 47, 40, 104, 18];
-        // let ex_str = String::from_utf8(exemple.to_vec()).unwrap();
-
-        // println!("sel d'exemple : {}", ex_str);
-        println!("mon_encrypt : {:?}", salt.to_vec());
-        println!("mon sel decrypt : {}", String::from_utf8(salt.to_vec()).unwrap());
-
+        let salt: [u8;16] = Self::salt_u8_16();
 
         let hashed = hash_with_salt(
             password,
             DEFAULT_COST,
             salt
-            // [38, 113, 212, 141, 108, 213, 195, 166, 201, 38, 20, 13, 47, 40, 104, 18]
         );
         hashed
             .unwrap()
