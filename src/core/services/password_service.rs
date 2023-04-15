9eb39464f8ccabcceb2c629cf66cd2b39d7be67d
paq1 <p.paquin1016@laposte.net>
2023-04-15 08:48:44 +0200

    chore: Send + Sync pour Password service

--- src/core/services/password_service.rs
+++ src/core/services/password_service.rs
@@ -1,4 +1,4 @@
-pub trait PasswordService {
+pub trait PasswordService: Send + Sync {
     fn create_hash_password(&self, password: String) -> String;
     fn verifie(&self, password: String, hash: String) -> bool;
 }
